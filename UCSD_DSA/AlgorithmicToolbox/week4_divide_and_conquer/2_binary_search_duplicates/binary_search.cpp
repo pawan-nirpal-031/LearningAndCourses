5bdc48d468c95909f3d5f1872c944d9756652af2
pawan-nirpal-031 <pawannirpal@gmail.com>
2022-09-29 19:30:45 +0530

    trivial dnc problems pushed

--- UCSD_DSA/AlgorithmicToolbox/week4_divide_and_conquer/2_binary_search_duplicates/binary_search.cpp
+++ UCSD_DSA/AlgorithmicToolbox/week4_divide_and_conquer/2_binary_search_duplicates/binary_search.cpp
@@ -1,36 +1,76 @@
-#include <iostream>
-#include <cassert>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long int ull;
+typedef long long int ll;
+typedef long double ld;
+#define Mod 1000000007
+#define Infinity (ll)1e18
+#define Append(a) push_back(a)
+#define Pair(a,b) make_pair(a,b)
+#define Clear(a) for(ll &x : a){x=0;}
+#define Point(x) std::fixed<<setprecision(15)<<x
+#define SetBits(x) __builtin_popcount(x);
+#define DebugCase(i,x) cout<<"Case #"<<i<<": "<<x<<'\n'
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define Status(b) (cout<<(b?"YES\n":"NO\n"));
 
-using std::vector;
 
-int binary_search(const vector<int> &a, int x) {
-  int left = 0, right = (int)a.size(); 
-  //write your code here
-}
-
-int linear_search(const vector<int> &a, int x) {
-  for (size_t i = 0; i < a.size(); ++i) {
-    if (a[i] == x) return i;
+int PassOne(int a[],int n,int q){
+  int l = 0; 
+  int h = n-1; 
+  while(l+1<h){
+    int m = (l+(h-l)/2);
+    if(a[m]==q) return m;
+    else if(q>a[m]){
+      l = m;
+    }else{
+      h = m;
+    }
   }
+  if(a[l]==q) return l;
+  if(a[h]==q) return h;
   return -1;
 }
 
-int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
+int PassTwo(int a[],int n,int x,int upper){
+  int h = upper;
+  int l =0; 
+  while(l+1<h){
+    int m = (l+(h-l)/2);
+    if(a[m]==x){
+      h = m;
+    }else l = m;
   }
-  int m;
-  std::cin >> m;
-  vector<int> b(m);
-  for (int i = 0; i < m; ++i) {
-    std::cin >> b[i];
-  }
-  for (int i = 0; i < m; ++i) {
-    //replace with the call to binary_search when implemented
-    std::cout << linear_search(a, b[i]) << ' ';
-  }
+  if(a[l]==x) return l;
+  return h;
 }
+
+int SinglePassSolve(int a[],int n,int x){
+  int h = n-1; 
+  int l = 0; 
+  while(l+1<h){
+    int m = (l + (h-l)/2);
+    if(x<=a[m]){
+      h = m;
+    }else l = m;
+  }
+  if(a[l]!=x and a[h]!=x) return -1;
+  else if(a[l]==x) return l; 
+  return h; 
+}
+
+int main(){
+  FastIO; 
+  int n;
+  cin>>n; 
+  int a[n];
+  for(int &x : a) cin>>x;
+  int q;
+  cin>>q; 
+  while(q--){
+    int x;
+    cin>>x;  
+    cout<<SinglePassSolve(a,n,x)<<' ';
+  }
+  return 0;
+} 
--- UCSD_DSA/AlgorithmicToolbox/week4_divide_and_conquer/5_number_of_inversions/inversions.cpp
+++ UCSD_DSA/AlgorithmicToolbox/week4_divide_and_conquer/5_number_of_inversions/inversions.cpp
@@ -1,25 +1,54 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long int ull;
+typedef long long int ll;
+typedef long double ld;
+#define Mod 1000000007
+#define Infinity (ll)1e18
+#define Append(a) push_back(a)
+#define Pair(a,b) make_pair(a,b)
+#define Clear(a) for(ll &x : a){x=0;}
+#define Point(x) std::fixed<<setprecision(15)<<x
+#define SetBits(x) __builtin_popcount(x);
+#define DebugCase(i,x) cout<<"Case #"<<i<<": "<<x<<'\n'
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define Status(b) (cout<<(b?"YES\n":"NO\n"));
 
-using std::vector;
+int inv_count = 0;
 
-long long get_number_of_inversions(vector<int> &a, vector<int> &b, size_t left, size_t right) {
-  long long number_of_inversions = 0;
-  if (right <= left + 1) return number_of_inversions;
-  size_t ave = left + (right - left) / 2;
-  number_of_inversions += get_number_of_inversions(a, b, left, ave);
-  number_of_inversions += get_number_of_inversions(a, b, ave, right);
-  //write your code here
-  return number_of_inversions;
-}
-
-int main() {
-  int n;
-  std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); i++) {
-    std::cin >> a[i];
+void combine(int a[],int st,int mid,int end){
+  int n = end - st + 1; 
+  int temp[n];
+  int t =0; 
+  int p1 = st; 
+  int p2 = mid+1;
+  while(p1<=mid and p2<=end){
+    if(a[p1]<=a[p2]) temp[t++] = a[p1++];
+    else{
+      inv_count+=(mid-p1+1);
+      temp[t++] = a[p2++];
+    }
   }
-  vector<int> b(a.size());
-  std::cout << get_number_of_inversions(a, b, 0, a.size()) << '\n';
+  if(p1<=mid) while(p1<=mid) temp[t++] = a[p1++];
+  if(p2<=end) while(p2<=end) temp[t++] = a[p2++];
+  for(int i = 0;i<n;i++) a[st+i] = temp[i];
 }
+
+void ms(int a[],int st,int end){
+  if(st==end or st>end) return; 
+  int mid = (st + (end-st)/2);
+  ms(a,st,mid);
+  ms(a,mid+1,end);
+  combine(a,st,mid,end);
+}
+
+int main(){
+  FastIO; 
+  int n;
+  cin>>n; 
+  int a[n];
+  for(int i =0;i<n;i++) cin>>a[i];
+  ms(a,0,n-1);
+  cout<<inv_count;
+  return 0;
+} 

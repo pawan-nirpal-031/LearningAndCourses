5012bdcedcb730692916a5805e7c35cc3c276c65
pawan-nirpal-031 <pawannirpal@gmail.com>
2022-12-01 14:14:42 +0530

    syncing main

--- UCSD_DSA/DataStructures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
+++ UCSD_DSA/DataStructures/week2_priority_queues_and_disjoint_sets/1_make_heap/build_heap.cpp
@@ -1,63 +1,45 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+typedef unsigned long long int ull;
+typedef long long int ll;
+typedef long double ld;
+#define Mod 1000000007
+#define Infinity (ll)1e18
+#define Append(a) push_back(a)
+#define Pair(a,b) make_pair(a,b)
+#define Clear(a) for(ll &x : a){x=0;}
+#define Point(x) std::fixed<<setprecision(15)<<x
+#define SetBits(x) __builtin_popcount(x);
+#define DebugCase(i,x) cout<<"Case #"<<i<<": "<<x<<'\n'
+#define FastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define Status(b) (cout<<(b?"YES\n":"NO\n"));
 
-using std::vector;
-using std::cin;
-using std::cout;
-using std::swap;
-using std::pair;
-using std::make_pair;
-
-class HeapBuilder {
- private:
-  vector<int> data_;
-  vector< pair<int, int> > swaps_;
-
-  void WriteResponse() const {
-    cout << swaps_.size() << "\n";
-    for (int i = 0; i < swaps_.size(); ++i) {
-      cout << swaps_[i].first << " " << swaps_[i].second << "\n";
-    }
+void ShiftDown(int a[],int i,int n,vector<pair<int,int>> &swaps){
+  int min_indx = i; 
+  int left = 2*i+1; 
+  int right = 2*i+2; 
+  if(left<n and a[left]<a[min_indx]) min_indx = left; 
+  if(right<n and a[right]<a[min_indx]) min_indx = right; 
+  if(i!=min_indx){
+    swap(a[i],a[min_indx]);
+    swaps.push_back({i,min_indx});
+    ShiftDown(a,min_indx,n,swaps);
   }
-
-  void ReadData() {
-    int n;
-    cin >> n;
-    data_.resize(n);
-    for(int i = 0; i < n; ++i)
-      cin >> data_[i];
-  }
-
-  void GenerateSwaps() {
-    swaps_.clear();
-    // The following naive implementation just sorts 
-    // the given sequence using selection sort algorithm
-    // and saves the resulting sequence of swaps.
-    // This turns the given array into a heap, 
-    // but in the worst case gives a quadratic number of swaps.
-    //
-    // TODO: replace by a more efficient implementation
-    for (int i = 0; i < data_.size(); ++i)
-      for (int j = i + 1; j < data_.size(); ++j) {
-        if (data_[i] > data_[j]) {
-          swap(data_[i], data_[j]);
-          swaps_.push_back(make_pair(i, j));
-        }
-      }
-  }
-
- public:
-  void Solve() {
-    ReadData();
-    GenerateSwaps();
-    WriteResponse();
-  }
-};
-
-int main() {
-  std::ios_base::sync_with_stdio(false);
-  HeapBuilder heap_builder;
-  heap_builder.Solve();
-  return 0;
 }
+
+void ConvertToHeap(int a[],int n){
+  vector<pair<int,int>>swaps; 
+  for(int i =n/2;i>=0;i--) ShiftDown(a,i,n,swaps); 
+  cout<<swaps.size()<<'\n';
+  for(auto &tuple : swaps) cout<<tuple.first<<' '<<tuple.second<<'\n';
+}
+
+int main(){
+  FastIO; 
+  int n;  
+  cin>>n;
+  int a[n];
+  for(int i =0;i<n;i++) cin>>a[i];
+  ConvertToHeap(a,n);
+  return 0;
+} 

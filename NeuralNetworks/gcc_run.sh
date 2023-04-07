bb29aa7631ffa97741d8701a702a46e2f1f685b9
pawan-nirpal-031 <pawannirpal@gmail.com>
2023-04-07 23:13:46 +0530

    adding python plotting, training plots generated

--- NeuralNetworks/gcc_run.sh
+++ NeuralNetworks/gcc_run.sh
@@ -4,3 +4,4 @@ make
 mv OpenANN bin
 ./bin/OpenANN
 #( find ./ -name '*.cpp' -print0 | xargs -0 cat ) | wc -l
+# g++ Gradient.cpp -std=c++11 -I/usr/include/python2.7  -I/home/pnirpal/.local/lib/python2.7/site-packages/numpy/core/include/  -lpython2.7
